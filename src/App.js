b3cec84ede0f16b505c797eeaa46d4cfbf2ca0c1
Ben Stones <benstones99@gmail.com>
2022-07-23 22:38:35 +0100

    WIP - usecontext example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import UseEffectExample from './components/useEffect';
 import UseStateExample from './components/useState'
+import UseContextExample from './components/useContext';
 
 function App() {
   return (
     <>
       <UseStateExample />
       <UseEffectExample />
+      <UseContextExample />
     </>
 
   );
