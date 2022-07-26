e9bf4d6d603df5823cf2f2f973a825d9d69de671
Ben Stones <benstones99@gmail.com>
2022-07-26 22:43:45 +0100

    Clean up use context

--- src/components/useContext.js
+++ src/components/useContext.js
@@ -3,7 +3,7 @@ import { useState, createContext, useContext } from "react";
 const UserContext = createContext();
 
 function UseContextExample() {
-    const [user, setUser] = useState("Jesse Hall");
+    const [user, setUser] = useState("Brad Pitt");
 
     return (
         <UserContext.Provider value={user}>
@@ -16,6 +16,7 @@ function UseContextExample() {
 function Component2() {
     return (
         <>
+            <hr/>
             <h5>Component 2</h5>
             <Component3 />
         </>
@@ -23,30 +24,12 @@ function Component2() {
 }
 
 function Component3() {
-    return (
-        <>
-            <h5>Component 3</h5>
-            <Component4 />
-        </>
-    );
-}
-
-function Component4() {
-    return (
-        <>
-            <h5>Component 4</h5>
-            <Component5 />
-        </>
-    );
-}
-
-function Component5() {
     const user = useContext(UserContext);
 
     return (
         <>  
             <hr />
-            <h5>Component 5</h5>
+            <h5>Component 3</h5>
             <h3>{`Hello ${user} again!`}</h3>
         </>
     );
