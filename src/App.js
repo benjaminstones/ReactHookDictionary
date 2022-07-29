9d42643771e6a2dd20edb9be552c9e090848aa40
Ben Stones <benstones99@gmail.com>
2022-07-29 17:34:28 +0100

    Added useRef + format

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import UseEffectExample from './components/useEffect';
 import UseStateExample from './components/useState'
 import UseContextExample from './components/useContext';
+import UseRefExample from './components/useRef';
 
 function App() {
   return (
     <>
       <UseStateExample />
+      <br/>
+      <hr/>
+      <br/>
       <UseEffectExample />
+      <br/>
+      <hr/>
+      <br/>
       <UseContextExample />
+      <br/>
+      <hr/>
+      <br/>
+      <UseRefExample />
+
     </>
 
   );
---
+++ src/components/useRef.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useRef, React } from "react";
+
+function UseRefExample() {
+    const [inputValue, setInputValue] = useState("");
+    const previousInputValue = useRef("");
+
+    useEffect(() => {
+        previousInputValue.current = inputValue;
+    }, [inputValue]);
+
+    return (
+        <>
+            <h1>useRef</h1>a
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <h2>Current Value: {inputValue}</h2>
+            <h2>Previous Value: {previousInputValue.current}</h2>
+        </>
+    );
+}
+
+export default UseRefExample;
